bedea568269e9b39f6318d1b174e40ce98be61dc
Chloe Seclier <seclier.chloe@gmail.com>
2025-02-23 17:02:39 +0100

    display tablProgram

--- client/src/pages/programs.tsx
+++ client/src/pages/programs.tsx
@@ -1,3 +1,42 @@
+import { useEffect, useState } from "react";
+
+interface Program {
+  id: number;
+  title: string;
+  synopsis: string;
+  poster: string;
+  country: string;
+  year: number;
+}
+
 export default function Programs() {
-  return <>coucou</>;
+  const [program, setProgram] = useState<Program[]>([]);
+
+  useEffect(() => {
+    fetch("http://localhost:3310/api/programs")
+      .then((response) => response.json())
+      .then((data) => setProgram(data))
+      .catch((error) => console.error("Erreur de chargement :", error));
+  }, []);
+
+  return (
+    <>
+      <p>coucou</p>
+      {program.length > 0 ? (
+        <>
+          <ul>
+            {" "}
+            Mon programme
+            {program.map((item) => (
+              <li key={item.id}>
+                {item.title} - {item.country}
+              </li>
+            ))}
+          </ul>
+        </>
+      ) : (
+        <p>Chargement...⏳</p>
+      )}
+    </>
+  );
 }
